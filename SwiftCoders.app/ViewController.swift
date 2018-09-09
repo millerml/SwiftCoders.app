c3aee20e53d72c9ac46c2640c33273c5c89004ee
Lewis Matthew Miller <ustereo@icloud.com>
2018-09-09 15:56:53 -0700

    Initial Commit

---
+++ SwiftCoders.app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCoders.app
+//
+//  Created by Lewis Matthew Miller on 9/9/18.
+//  Copyright © 2018 SwiftCoders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
